bdf68e3bda003c995a97842ad0e07de728208f55
txusdavid27 <jetraslavina@javeriana.edu.co>
2023-05-24 02:58:52 -0500

    Modificaciones en Modulo Auth, siguen los guards

--- Capa-Presentacion/client-app/src/app/modules/auth/auth.module.ts
+++ Capa-Presentacion/client-app/src/app/modules/auth/auth.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginPopupComponent } from './pages/login-popup/login-popup.component';
+
+
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
