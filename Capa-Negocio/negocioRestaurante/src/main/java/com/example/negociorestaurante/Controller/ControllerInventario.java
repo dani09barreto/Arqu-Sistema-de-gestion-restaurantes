da45711472da3a27befb2f9a607ddb41ef429f9e
SantiagoGV29 <gu_santiago@javeriana.edu.co>
2023-05-27 13:06:17 -0500

    Agrego service locator content

---
+++ Capa-Negocio/negocioRestaurante/src/main/java/com/example/negociorestaurante/Controller/ControllerInventario.java
@@ -0,0 +1,5 @@
+package com.example.negociorestaurante.Controller;
+
+public class ControllerInventario {
+    
+}
--- Capa-Negocio/negocioRestaurante/src/main/java/com/example/negociorestaurante/ServiceLocator/IServiceLocator.java
+++ Capa-Negocio/negocioRestaurante/src/main/java/com/example/negociorestaurante/ServiceLocator/IServiceLocator.java
@@ -5,15 +5,22 @@ import org.springframework.stereotype.Service;
 
 import javax.naming.NamingException;
 import java.io.IOException;
+import java.rmi.server.ExportException;
 
 @Service
 public interface IServiceLocator {
-    IRemoteBodegaService getRemoteBodegaService() throws Exception;
-    IRemoteCantidadIngredienteService getRemoteCantidadIngredienteService();
-    IRemoteComentarioRestauranteService getRemoteComentarioRestauranteService();
-    IRemoteEstadoEnvioService getRemoteEstadoEnvioService();
-    IRemoteIngredientePlatoService getRemoteIngredientePlatoService() throws NamingException, IOException;
+    IRemoteAdicionalesService getRemoteAdicionalesService() throws Exception;
+    IRemoteClienteService getRemoteClienteService() throws Exception;
+    IRemoteDescuentoFidelidadService getRemoteDescuentoFidelidadService() throws Exception;
+    IRemoteEstadoPedidoService getRemoteEstadoPedidoService() throws Exception;
+    IRemoteIngredienteService getRemoteIngredienteService() throws Exception;
+    IRemoteInventarioService getRemoteInventarioService() throws Exception;
+    IRemoteMesaService getRemoteMesaService() throws Exception;
+    IRemotePagoService getRemotePagoService() throws Exception;
+    IRemotePedidoService getRemotePedidoService() throws Exception;
+    IRemotePlatoPedidoService getRemotePlatoPedidoService() throws Exception;
+    IRemoteRegistroPagoService getRemoteRegistroPagoService() throws Exception;
+    IRemoteTipoIngredienteService getRemoteTipoIngredienteService() throws Exception;
+    IRemoteTipoPagoService getRemoteTipoPagoService()throws Exception;
     IRemoteUsuarioService getRemoteUsuarioService() throws NamingException, IOException;
-    IRemoteRoleService getRemoteRoleService() throws NamingException, IOException;
-
 }
